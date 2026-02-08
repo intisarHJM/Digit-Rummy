54249a9fc4c78a3717468acd8898290569f48ce0
Intisar Hussain Jaafar <Intesarabdullah67@gmail.com>
2026-02-08 09:18:30 +0300

    initial layout style is done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello sweaty")
+const tails = document.querySelectorAll('.tiles')
+console.log(tails)
+
+//.player-rack -> #player-1-rack | #player-2-rack
+//stockpile
+//.tile-group
+//main-board
+
